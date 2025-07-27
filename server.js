254ad65f7399a676c9715f2e22eac05b71d4e1c9
SuMayaBee <sumaiyaislam-14-2021711385@it.du.ac.bd>
2025-07-27 07:53:18 +0600

    Checking

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "Hello, SuMayaBee. You are awesome !!!"
+        message: "Hello, SuMayaBee. You can do it, you are awesome !!!"
     })
 })
 
 app.listen(8080, () => {
     console.log('server running on port 8080')
 })
+
+
+// sudo ./svc.sh install
+// sudo ./svc.sh install
+// sudo ./svc.sh status
